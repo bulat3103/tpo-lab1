9aa64171a018007ee0bdeef4564bb74766ed4394
admin <hafizov.bulat2002@gmail.com>
2023-03-04 13:44:04 +0300

    Fixes

--- src/main/java/com/example/tpolab1/task2/ComparisonSort.java
+++ src/main/java/com/example/tpolab1/task2/ComparisonSort.java
@@ -1,16 +1,46 @@
 package com.example.tpolab1.task2;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ComparisonSort {
-    public static int[] insertionSort(int[] sortArr) {
+    public static String[] insertionSort(int[] sortArr) {
         if (sortArr == null) throw new NullPointerException();
+        if (sortArr.length == 0) return new String[]{};
+        Map<Integer, int[]> steps = new HashMap<>();
+        steps.put(1, Arrays.copyOf(sortArr, sortArr.length));
+        int step = 1;
         int j;
         for (int i = 1; i < sortArr.length; i++) {
             int swap = sortArr[i];
             for (j = i; j > 0 && swap < sortArr[j - 1]; j--) {
                 sortArr[j] = sortArr[j - 1];
+                sortArr[j - 1] = swap;
+                steps.put(step++, Arrays.copyOf(sortArr, sortArr.length));
             }
-            sortArr[j] = swap;
         }
-        return sortArr;
+        String[] ans = new String[steps.size()];
+        int i = 0 ;
+        for (Map.Entry<Integer, int[]> e : steps.entrySet()) {
+            StringBuilder line = new StringBuilder();
+            for (int k = 0; k < e.getValue().length; k++) {
+                if (k == e.getValue().length - 1) {
+                    line.append(e.getValue()[k]);
+                } else {
+                    line.append(e.getValue()[k]).append(" ");
+                }
+            }
+            ans[i++] = line.toString();
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int [] a = new int[]{0,0,0,0,0};
+        String[] steps = insertionSort(a);
+        for (int i = 0; i < steps.length; i++) {
+            System.out.println(steps[i]);
+        }
     }
 }
--- src/test/java/com/example/tpolab1/task2/ComparisonSortTest.java
+++ src/test/java/com/example/tpolab1/task2/ComparisonSortTest.java
@@ -9,9 +9,9 @@ public class ComparisonSortTest {
     @Test
     public void checkPositiveSort() {
         assertAll(
-                () -> assertArrayEquals(new int[]{3, 4, 5, 33}, ComparisonSort.insertionSort(new int[]{33, 3, 4, 5})),
-                () -> assertArrayEquals(new int[]{1,3,4,7,9}, ComparisonSort.insertionSort(new int[]{4,1,3,9,7})),
-                () -> assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9,10}, ComparisonSort.insertionSort(new int[]{10,9,8,7,6,5,4,3,2,1}))
+                () -> assertArrayEquals(new String[]{"3 33 4 5", "3 4 33 5", "3 4 5 33"}, ComparisonSort.insertionSort(new int[]{33, 3, 4, 5})),
+                () -> assertArrayEquals(new String[]{"9 10 8 7 6", "9 8 10 7 6", "8 9 10 7 6", "8 9 7 10 6", "8 7 9 10 6",
+                        "7 8 9 10 6", "7 8 9 6 10", "7 8 6 9 10", "7 6 8 9 10", "6 7 8 9 10"}, ComparisonSort.insertionSort(new int[]{10,9,8,7,6}))
         );
     }
 
@@ -22,16 +22,16 @@ public class ComparisonSortTest {
 
     @Test
     void checkNegativeValues() {
-        assertArrayEquals(new int[]{-5, -4, -3, -2, -1}, ComparisonSort.insertionSort(new int[]{-5, -1, -4, -3, -2}));
+        assertArrayEquals(new String[]{"-5 -4 -1 -3 -2", "-5 -4 -3 -1 -2", "-5 -4 -3 -2 -1"}, ComparisonSort.insertionSort(new int[]{-5, -1, -4, -3, -2}));
     }
 
     @Test
     public void checkZeros() {
-        assertArrayEquals(new int[]{0,0,0,0,0}, ComparisonSort.insertionSort(new int[]{0,0,0,0,0}));
+        assertArrayEquals(new String[]{"0 0 0 0 0"}, ComparisonSort.insertionSort(new int[]{0,0,0,0,0}));
     }
 
     @Test
     public void checkEmpty() {
-        assertArrayEquals(new int[]{}, ComparisonSort.insertionSort(new int[]{}));
+        assertArrayEquals(new String[]{}, ComparisonSort.insertionSort(new int[]{}));
     }
 }
